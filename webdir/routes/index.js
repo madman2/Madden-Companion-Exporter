146d4de29b1fd921c94b870e54648ed7df43786d
Matthew Dorsett <matthew.a.dorsett@gmail.com>
2018-01-21 20:58:29 -0800

    Add GET route to retrieve data

--- webdir/routes/index.js
+++ webdir/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var admin = require('../firebase/firebase-admin')
 var router = express.Router();
+var db = admin.database();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Dr. Fugurgal Madden Data' });
+});
+
+router.get('/data', function(req, res, next) {
+  res.json(db.ref());
 });
 
 module.exports = router;
