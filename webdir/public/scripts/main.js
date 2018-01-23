451093159e0aa48e339bde9b9b7bb53c55b9cf7f
Matthew Dorsett <matthew.a.dorsett@gmail.com>
2018-01-22 19:26:03 -0800

    Add free agents

--- webdir/public/scripts/main.js
+++ webdir/public/scripts/main.js
@@ -1,27 +1,48 @@
 var dataTable;
 
 $(document).ready(function() {
-    showLeagueTeams();
+    showData('freeagents');
 });
 
-function showLeagueTeams() {
-    $.getJSON('/leagueteams').then(function(data) {
+function showData(request) {
+    $.getJSON('/' + request).then(function(data) {
 
         var columnNames = Object.keys(data['0']);
         var titlesArr = [];
 
         $.each(columnNames, function() {
-            titlesArr.add( { title: this });
+            titlesArr.push( { title: camelCaseToTitle(this) } );
         });
 
         dataTable = $('#dataTable').DataTable( {
             columns: titlesArr
         });
 
-        // $.each(data, function() {
-        //     teamsTable.row.add(this.);
-        // });
+        $.each(data, function() {
+            dataTable.row.add(Object.values(this));
+        });
 
-        teamsTable.draw();
+        dataTable.draw();
     });
+}
+
+function camelCaseToTitle(category) {
+    var transitions = [];
+    for (var i = 1; i < category.length; ++i) {
+        var c1 = category.charAt(i - 1);
+        var c2 = category.charAt(i);
+        if (c1 == c1.toLowerCase() && c2 == c2.toUpperCase()) {
+            transitions.push(i);
+        }
+    }
+
+    var title = category.charAt(0).toUpperCase();
+    var counter = 1;
+    for (var j = 0; j < transitions.length; ++j) {
+        title = title.concat(category.substring(counter, transitions[j]), ' ');
+        counter = transitions[j];
+    }
+    title = title.concat(category.substring(counter));
+
+    return title;
 }
